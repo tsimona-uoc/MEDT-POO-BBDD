909af9077b10d2ac063c7fad3dd4254df5b6f4ca
rusterboy77 <37505980+rusterboy77@users.noreply.github.com>
2025-11-21 17:37:52 +0100

    Excepciones

--- src/main/java/MEDT/MEDT/DAO/ConnectionUtil.java
+++ src/main/java/MEDT/MEDT/DAO/ConnectionUtil.java
@@ -8,7 +8,7 @@ public class ConnectionUtil {
 
     private static final String URL = "jdbc:mysql://localhost:3306/MEDT_POO_DDBB";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "17071981MCRmcr";
 
     static {
         try {
